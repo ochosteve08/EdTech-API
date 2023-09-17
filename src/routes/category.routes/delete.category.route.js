87e6ec1f875775ab1eacbcb14710740af258283c
ochosteve <ochosteve08@gmail.com>
2023-09-17 17:32:54 +0100

    update category route

--- src/routes/category.routes/delete.category.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { categoryControllers } = require('../../controllers');
-
-const deleteCategoryRoutes = express.Router();
-deleteCategoryRoutes.delete('/:id', categoryControllers.deleteCategoryById);
-
-module.exports = {
-  deleteCategoryRoutes,
-};
